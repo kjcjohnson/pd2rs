dafca68b22faf12e8a5eb2d16d4037ab2e81115c
packrat386 <packrat386@gmail.com>
2014-01-18 08:49:15 -0500

    normalized data

--- test.pl
+++ test.pl
@@ -1,8 +1,5 @@
-$hello = "|R2W1team=rox |R2W1score=0 |R2W1win=";
+$team1 = "hello";
 
+"hi";
 
-if($hello =~ /\|R([0-9]+)([A-Z])[0-9]+team=(.*)\s\|R[0-9]+[A-Z][0-9]+score=([0-9])/){
-    print ("yep \n");
-}else{
-    print ("nope\n");
-}
+print("$team1\n");
